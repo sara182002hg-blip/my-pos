4e8bf445339ff1d717dfc8e52f0f70ea084421d3
sara182002hg-blip <sara.182002.hg@gmail.com>
2026-01-02 01:20:10 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import requests
 import segno
 
 # ใส่ URL ที่ก๊อปปี้มาจาก Apps Script ตรงนี้
-API_URL = "https://script.google.com/macros/s/AKfycbxrTacD8Bes_H2WU_hfEAEbVZ61yOXbEMalb7frL_c8P751ZmbP7ZK23dWz1UrRVszj/exec"
+API_URL = "https://script.google.com/macros/s/AKfycbyMUkHtPHjusc714sbBJ9j1IZsYAOT9bB2geHRyA_KqqE-xXjpxXfKv7HJvb9TSmOav/exec"
 
 st.set_page_config(page_title="My POS with Images", layout="wide")
 
@@ -57,6 +57,7 @@ with col2:
                 st.session_state.cart = [] # ล้างตะกร้า
             else:
                 st.error("เกิดข้อผิดพลาดในการเชื่อมต่อ")
+
 
 
 
